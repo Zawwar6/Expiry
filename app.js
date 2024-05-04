6703ba8055dd705a692cfd4e706f0d8886bbfba0
Zawwar Khatri <159942012+Zawwar6@users.noreply.github.com>
2024-05-04 18:23:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// document.getElementById("calculateBtn").addEventListener("click", function() {
+//     var days = prompt("Enter the number of days:");
+    
+//     if (days !== null && days !== "") {
+//       var currentDate = new Date();
+//       var expirationDate = new Date(currentDate.getTime() + days * 24 * 60 * 60 * 1000);
+      
+//       var formattedExpirationDate = expirationDate.toLocaleDateString();
+      
+//       alert("Expiration Date: " + formattedExpirationDate);
+//     } else {
+//       alert("Please enter a valid number of days.");
+//     }
+//   });
+function myClick() {
+    var days = prompt("Enter the number of days:");
+    
+    if (days !== null && days !== "") {
+      var currentDate = new Date();
+      var expirationDate = new Date(currentDate.getTime() + days * 24 * 60 * 60 * 1000);
+      
+      var formattedExpirationDate = expirationDate.toLocaleDateString();
+      
+      alert("Expiration Date: " + formattedExpirationDate);
+    } else {
+      alert("Please enter a valid number of days.");
+    }}
